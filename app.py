4a83115472a7427c2435e6976e94525b793a3f75
KillerKelvUK <kelvin.middleton@gmail.com>
2024-06-12 20:45:51 +0100

    Code clean up.

--- app.py
+++ app.py
@@ -18,17 +18,9 @@ app = Flask(__name__, template_folder='.')
 def index():
     """Video streaming home page."""
     return render_template('index.html')
-    #return Response(gen(Camera()),
-    #                mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/video_feed')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/video_feed2')
-def video_feed2():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
