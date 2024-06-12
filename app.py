33e93f730f1e2735cabd17598ac266a3d15d7973
KillerKelvUK <kelvin.middleton@gmail.com>
2024-06-12 15:14:43 +0100

    Updating Flask code to remove need for templates sub-dir.

--- app.py
+++ app.py
@@ -12,13 +12,20 @@ from flask import Flask, render_template, Response
 # Bypass checking for environment variable and hardcode opencv
 Camera = import_module('camera_opencv').Camera
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 @app.route('/')
 def index():
     """Video streaming home page."""
-    # index.html needs to be in template\index.html or alter the Flash app definition
-    return render_template('index.html') 
+    return render_template('index.html')
+    #return Response(gen(Camera()),
+    #                mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def gen(camera):
     """Video streaming generator function."""
@@ -26,12 +33,6 @@ def gen(camera):
     while True:
         frame = camera.get_frame()
         yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
-
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
