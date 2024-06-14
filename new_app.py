fa64c023c2ed365deb6c2ced29daf93ddbc2c872
KillerKelvUK <kelvin.middleton@gmail.com>
2024-06-14 10:44:26 +0000

    Moved docker to a python slim image, updated pip requirements to opencv-python-headless to optimise for container and cleaned remaining code as new baseline for merge into main.

--- new_app.py
+++ new_app.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, Response
-# import threading
-# import datetime
-# import time
 import cv2
 
-# outputFrame = None
-# lock = threading.Lock()
 app = Flask(__name__, template_folder='.')
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 video_source = "rtsp://192.168.1.99:8554/blackprostation"
@@ -14,7 +9,6 @@ vs = cv2.VideoCapture(video_source)
 
 def genframes():    
     while True:
-        # read current frame
         _, img = vs.read()
 
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -24,7 +18,6 @@ def genframes():
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
         
-        # encode as a jpeg image and return it
         yield b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpg', img)[1].tobytes() + b'\r\n--frame\r\n'
 
 @app.route('/')
