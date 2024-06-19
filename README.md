49659b29a9e01be8fbe2162fb47fe89cbedefd90
= <=>
2024-06-19 22:51:34 +0100

    Updated visualisations.  Removed image size constraint.  Updated other logics to simplify.

--- README.md
+++ README.md
@@ -5,19 +5,29 @@ Real-time camera monitoring, face detection and recognition inside a docker cont
 * **archived_code/v1:**   Hack of Miguel Grinberg's and PORNPASOK SOOKYEN's code pulling frames off my webcam using go2rtc and streaming to a browser session using Flask
 * **archived_code/v2:**   Face detection using Haar cascades from OpenCV
 * **archived_code/v3:**   Face detection using YuNet with options to pass in either a still image or a video with a matching output including detection visualisation
-* **current branch:**     Face detection and recognition using YuNet and SFace with video output including basic recognition visualisation
+* **current branch:**     Face detection and recognition using YuNet and SFace with video output including ~~basic~~ recognition visualisation. Code also runs either in Windows with windowed real-time output or in a docker container with saved .mp4 video output.
 
 ### To Do
 The ever chaning goal posts!
 - [x] Implement sface recognition :beer:
-- [ ] Add more recognition visualisation data to the output video
-- [ ] Optimise resource impacts ensuring OpenCL usage.  Review OpenVINO for any tangible gain.
+- [x] Add more recognition visualisation data to the output video :beers:
 - [ ] Parse detection and recognition outputs into more human readable text
 - [ ] Implementing appropriate Threading
 - [ ] Implement Collections/Persons lookup in the recognition process
+- [ ] Optimise resource impacts ensuring OpenCL usage.  Review OpenVINO for any tangible gain.
 - [ ] Implement API and/or MQTT wrapper considering integration with the Home Assistant and Node Red projects
 
+
 My dev environment uses a USB Logitech Brio webcam on my workstation streamed into my container host/container using a great project credited below called go2rtc.  I'm very much looking to keep this small and lightweight and will optimise the processing overheads as best I can.  My end-state usecase is to process either jpeg snapshots or an rtsp feed from my Unifi G4 Doorbell camera for person and face detection and ultimately recogition.  I want to integrate this with Home Assistant and/or Node-Red to onward process the recognition output into events and alerts with my vairous IoT devices.
+
+### Observations
+My container host is an unraid server with a Intel Core i5-14500.  I have a USB connected PSU where unraid is polling the bus and reporting various stats including current power draw.  The table below is an early data capture of the code run cost.
+| Power (watts) | Scenario |
+| --- | --- |
+| 76w | Container host baseline |
+| 86w (+10w) | Running code but with no detections/recognitions |
+| 103w (+27w-c) | 1 face detected |
+| 119w (+43w-c) | 2 faces detected |
 
 ### Fair warning
 I'm very much learning and have little experience with opencv, python & coding in general.  Any advice e.g. "read up on this..." or "check this code/link out..." is welcomed!
--- app.py
+++ app.py
@@ -26,26 +26,35 @@ def get_os():
     
 print('Running in', get_os())
 
+def draw_text(query_image, text, pos=(0, 0), font=cv.FONT_HERSHEY_SIMPLEX, font_scale=0.5, text_color=(255, 255, 255), font_thickness=1, text_color_bg=(0, 0, 0), border=1):
+    x, y = pos
+    text_size, _ = cv.getTextSize(text, font, font_scale, font_thickness)
+    text_w, text_h = text_size
+    cv.rectangle(query_image, (x, y + border), (x + text_w, y - text_h - border), text_color_bg, -1)
+    cv.putText(query_image, text, (x, y), font, font_scale, text_color, font_thickness)
+
+    return #text_size
+
 def visualize(query_image, query_faces, matches, scores, fps, detection_time, inference_time): # target_size: (h, w)
     matched_box_color = (0, 255, 0)    # BGR
     mismatched_box_color = (0, 0, 255) # BGR
-    text_color = (0, 0, 255)
-
+        
     # Validate results
     assert query_faces.shape[0] == len(matches), "number of query_faces needs to match matches"
     assert len(matches) == len(scores), "number of matches needs to match number of scores"
     
-    cv.putText(query_image,f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S}", (0, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1)
+    draw_text(query_image, text=f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S}", pos=(0, 30))
+    #cv.putText(query_image,f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S}", (0, 30), cv.FONT_HERSHEY_SIMPLEX, 0.4, text_color, 1)
 
     if fps is not None:
-        cv.putText(query_image, 'FPS: {:.2f}'.format(fps), (0, 45), cv.FONT_HERSHEY_SIMPLEX, 0.4, text_color)
+        draw_text(query_image, text=f"FPS: {fps:.0f}", pos=(0, 45))
+        #cv.putText(query_image, 'FPS: {:.2f}'.format(fps), (0, 45), cv.FONT_HERSHEY_SIMPLEX, 0.4, text_color, 1)
 
-    if detection_time is not None:
-        cv.putText(query_image,f"Detection Time: {detection_time:.4f}ms", (0, 60), cv.FONT_HERSHEY_SIMPLEX, 0.4, text_color)    
+    # Only report detection time if there are faces found in the frame:
+    if query_faces.shape[0] > 0:
+        draw_text(query_image, text=f"Detection: {detection_time:.4f}ms", pos=(0, 60))
+        #cv.putText(query_image,f"Detection: {detection_time:.4f}ms", (0, 60), cv.FONT_HERSHEY_SIMPLEX, 0.4, text_color)    
 
-    if inference_time is not None:
-        cv.putText(query_image,f"Inference Time: {inference_time:.4f}ms", (0, 75), cv.FONT_HERSHEY_SIMPLEX, 0.4, text_color)
-    
     # Draw bbox
     for index, match in enumerate(matches):
         bbox = query_faces[index][:4]
@@ -55,6 +64,7 @@ def visualize(query_image, query_faces, matches, scores, fps, detection_time, in
 
         score = scores[index]
         text_color = matched_box_color if match else mismatched_box_color
+        draw_text(query_image, text=f"Inference: {inference_time:.4f}ms", pos=(0, 75))
         cv.putText(query_image, "{:.2f}".format(score), (x, y - 5), cv.FONT_HERSHEY_DUPLEX, 0.4, text_color)
 
     return query_image
@@ -86,64 +96,64 @@ if target_face.shape[0] == 0:
 # Load query video source
 query_source_url = "rtsp://192.168.1.99:8554/blackprostation"
 #query_source_url = "rtsps://192.168.1.1:7441/EOEohGh0eoXIWf28"
-#query_source_url = "rtsp://localhost:8554/frontdoor_mid"
+#query_source_url = "event.mp4"
+
 query_source = cv.VideoCapture(query_source_url)
 query_source_width = int(query_source.get(cv.CAP_PROP_FRAME_WIDTH))
 query_source_height = int(query_source.get(cv.CAP_PROP_FRAME_HEIGHT))
-print("Query Source:", query_source_width, query_source_height)
 
+# ONLY FOR MY DEV ENV
 if get_os() == "Linux":
     output = cv.VideoWriter('/mnt/cache/processing/result.mp4', cv.VideoWriter_fourcc(*'mp4v'), 25, (query_source_width, query_source_height), True)
 elif get_os() == "Windows":
     output = cv.VideoWriter('result.mp4', cv.VideoWriter_fourcc(*'mp4v'), 25, (query_source_width, query_source_height), True)
 
-fps=cv.TickMeter()
-
+fps = cv.TickMeter()
+frame_skip = 2
 try:
     while True:
         # read a frame from the query video source
         fps.start()
         hasFrame, query_image = query_source.read()
+        if not hasFrame:
+            sys.exit("Unable to grab a frame from query_source")
 
         # Attempt to detect faces
-        detector.setInputSize([query_image.shape[1], query_image.shape[0]])
-       
-        
         det_start_time = time.time()
+        detector.setInputSize([query_image.shape[1], query_image.shape[0]])
         query_faces = detector.infer(query_image)
         det_end_time = time.time()
         
+        detection_time = det_end_time - det_start_time
 
-        # If no faces detected just move to the next frame and retry
-        # if query_faces.shape[0] == 0:
-        #     print('No faces deteceted in query source')
-        #     continue
-        
-        # Match
+        # Match faces to target
         scores = []
         matches = []
+        infer_start_time = time.time()
         for face in query_faces:
-            infer_start_time = time.time()
             result = recognizer.match(target_image, target_face[0][:-1], query_image, face[:-1])
             scores.append(result[0])
             matches.append(result[1])
-            infer_end_time = time.time()
-
-        # Draw results
-        fps.stop()
-        detection_time = det_end_time - det_start_time
+        
+        infer_end_time = time.time()
+            
         inference_time = infer_end_time - infer_start_time
+        
+        fps.stop()
+        
+        # Draw results
         image = visualize(query_image, query_faces, matches, scores, fps.getFPS(), detection_time, inference_time)
         
         if get_os() == "Linux":
-            #output.write(image)
-            pass
+            output.write(image)
         elif get_os() == "Windows":
             cv.imshow('autoface', image)
-            #output.write(image)
             if cv.waitKey(1) == ord('q'):
                 break
         
+        for _ in range(frame_skip - 1):
+            query_source.read()
+
 except KeyboardInterrupt:
     pass
 
